ae001cdd82e1a4f0caf35f9b1fe8291cdda4a4e1
손성민 <alexzzanga7407@gmail.com>
2025-04-14 00:10:20 -0400

    Update Simulation Service - RunIncomeEvents & PayExpenseAndTax

--- src/main/java/com/app/lifetimefinancialplanner/service/SimulationService.java
+++ src/main/java/com/app/lifetimefinancialplanner/service/SimulationService.java
@@ -1,7 +1,10 @@
 package com.app.lifetimefinancialplanner.service;
 
+import com.app.lifetimefinancialplanner.domain.context.SimulationContext;
 import com.app.lifetimefinancialplanner.domain.dto.SimulationDTO;
+import com.app.lifetimefinancialplanner.domain.entity.Scenario;
 
 public interface SimulationService {
     SimulationDTO runSimulation(Long scenarioId, Integer simulationCount);
+    void payExpenseAndTax(Scenario scenario, SimulationContext context, Boolean userAlive, Boolean spouseAlive);
 }
