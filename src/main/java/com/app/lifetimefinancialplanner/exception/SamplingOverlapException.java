9f9a815aaf68a07e5e3a98f6e7f9559c63497ab3
손성민 <alexzzanga7407@gmail.com>
2025-05-03 17:24:53 -0400

    Fixed RunInvestEvent Components

---
+++ src/main/java/com/app/lifetimefinancialplanner/exception/SamplingOverlapException.java
@@ -0,0 +1,7 @@
+package com.app.lifetimefinancialplanner.exception;
+
+public class SamplingOverlapException extends RuntimeException {
+    public SamplingOverlapException(String message) {
+        super(message);
+    }
+}
